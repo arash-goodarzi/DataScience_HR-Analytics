86b7352f5fe09d4c250febc5eb09e426e4294887
Arash <aras.goodarzi@gmail.com>
2022-01-21 10:54:48 -0500

    accept custom page for fetching

--- phase-1-scraping-glassdoor.py
+++ phase-1-scraping-glassdoor.py
@@ -1,31 +1,37 @@
-import os
 import datetime
+import os
+
 import pandas as pd
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 # Constants
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 
 # URLSearchParams
-def pageGlassDoorScraping(url):
+def page_glassdoor_scraping_selenium(url):
     driver = webdriver.Chrome(service=Service(PATH), options=webdriver.ChromeOptions())
     result = []
 
     driver.maximize_window()
     driver.get(url)
 
+    try:
+        job_age_list_ = driver.find_elements(By.XPATH, '//div[@data-test="job-age"]')
+        job_age_list = [day.text for day in job_age_list_]
+    except NoSuchElementException:
+        job_age_list = []
     elements = driver.find_elements(By.XPATH, '//li[@data-id]')
 
     # Header table for debugging
     print('*'*15, " Start the process ", '*'*15)
     print('|company_name|rate|city|state|job_title|salary|salary_estimate|salary_avg|company_size|company_type'
-          '|company_sector|company_founded|company_industry|pros_txt|cons_txt|')
+          '|company_sector|company_founded|company_industry|descriptions_txt|descriptions_txt|job_age|pros_txt|cons_txt|')
     for index, element in enumerate(elements):
-        driver.implicitly_wait(2)
+        driver.implicitly_wait(3)
 
         try:
             element.click()
@@ -34,20 +40,24 @@ def pageGlassDoorScraping(url):
             continue
 
         try:
-            driver.find_element(By.XPATH, '//span[@alt="Close"]').click()
+            button_close = driver.find_element(By.XPATH, '//span[@alt="Close"]')
+            driver.execute_script("arguments[0].click();", button_close)
         except NoSuchElementException:
             pass
         finally:
-            driver.implicitly_wait(7)
+            driver.implicitly_wait(8)
 
-        # Take elements
-        company_name_rate = driver.find_element(By.XPATH,
-                                                  '//*[@id="JDCol"]/div/article/div/div[1]/div/div/div[1]/div[3]/div['
-                                                  '1]/div[1]').text
-        name_and_rate = company_name_rate.partition('\n')
-        company_name = name_and_rate[0].strip()
+        try:
+            # Take elements
+            company_name_rate = driver.find_element(By.XPATH,
+                                                      '//*[@id="JDCol"]/div/article/div/div[1]/div/div/div[1]/div[3]/div['
+                                                      '1]/div[1]').text
+            name_and_rate = company_name_rate.partition('\n')
+            company_name = name_and_rate[0].strip()
 
-        remote_work = False
+            remote_work = False
+        except NoSuchElementException:
+            company_name = ""
 
         try:
             rate = driver.find_element(By.XPATH, '//*[@id="JDCol"]/div/article/div/div[1]/div/div/div[1]/div[3]/div['
@@ -59,49 +69,53 @@ def pageGlassDoorScraping(url):
             company_size = driver.find_element(By.XPATH, '//*[@id="EmpBasicInfo"]/div[1]/div/div[1]/span[2]').text
         except NoSuchElementException:
             company_size = -1
+
         try:
             company_type = driver.find_element(By.XPATH, '//*[@id="EmpBasicInfo"]/div[1]/div/div[3]/span[2]').text
         except NoSuchElementException:
-            company_type = -1
+            company_type = ""
 
         try:
             company_sector = driver.find_element(By.XPATH, '//*[@id="EmpBasicInfo"]/div[1]/div/div[5]/span[2]').text
         except NoSuchElementException:
-            company_sector = -1
+            company_sector = ""
 
         try:
             company_founded = driver.find_element(By.XPATH, '//*[@id="EmpBasicInfo"]/div[1]/div/div[2]/span[2]').text
         except NoSuchElementException:
-            company_founded = -1
+            company_founded = ""
+
         try:
             company_industry = driver.find_element(By.XPATH, '//*[@id="EmpBasicInfo"]/div[1]/div/div[4]/span[2]').text
         except NoSuchElementException:
-            company_industry = -1
+            company_industry = ""
 
         try:
             location = driver.find_element(By.XPATH,
                                            '//*[@id="JDCol"]/div/article/div/div[1]/div/div/div[1]/div[3]/div[1]/div[3]').text
             location = location.split(", ")
         except NoSuchElementException:
-            location = -1
+            location = ""
 
         try:
+            print(location[0])
             city = location[0]
             if city == 'Remote':
                 remote_work = True
-                city = -1
+                city = ""
         except IndexError:
-            city = -1
+            city = ""
+
         try:
             state = location[1]
         except IndexError:
-            state = -1
+            state = ""
 
         try:
             job_title = driver.find_element(By.XPATH,
                                             '//*[@id="JDCol"]/div/article/div/div[1]/div/div/div[1]/div[3]/div[1]/div[2]').text
         except NoSuchElementException:
-            job_title = -1
+            job_title = ""
 
         try:
             salary = driver.find_element(By.XPATH, '//*[@id="JDCol"]//span[@data-test="detailSalary"]').text
@@ -129,7 +143,24 @@ def pageGlassDoorScraping(url):
             cons = driver.find_elements(By.XPATH, '//*[@id="Reviews"]/div/div/div[2]/p')
             cons_txt = [con.text for con in cons]
         except NoSuchElementException:
-            cons = -1
+            cons = ""
+
+        try:
+            descriptions_txt = driver.find_elements(By.XPATH, '//div[@class="jobDescriptionContent desc"]//p')
+            descriptions_txt = [desc.text for desc in descriptions_txt]
+        except NoSuchElementException:
+            descriptions_txt = ""
+
+        try:
+            descriptions_list = driver.find_elements(By.XPATH, '//div[@class="jobDescriptionContent desc"]//li')
+            descriptions_list = [con.text for con in descriptions_list]
+        except NoSuchElementException:
+            descriptions_list = ""
+
+        try:
+            job_age = job_age_list[index]
+        except NoSuchElementException:
+            job_age = -1
 
         #  pre-Cleaning Data
         salary = salary.partition("(")[0].strip()
@@ -143,7 +174,7 @@ def pageGlassDoorScraping(url):
         # Debugging Propose
 
         print(
-            f'|{company_name}|{rate}|{city}|{state}|{job_title}|{salary}|{salary_estimate}|{salary_avg}|{company_size}|{company_type}|{company_sector}|{company_founded}|{company_industry}|{pros_txt}|{cons_txt}|')
+            f'|{company_name}|{rate}|{city}|{state}|{job_title}|{salary}|{salary_estimate}|{salary_avg}|{company_size}|{company_type}|{company_sector}|{company_founded}|{company_industry}|{descriptions_txt}|{descriptions_list}|{job_age}|{pros_txt}|{cons_txt}|')
 
         # Add result to list
         result.append({
@@ -161,6 +192,9 @@ def pageGlassDoorScraping(url):
             "company_sector": company_sector,
             "company_founded": company_founded,
             "company_industry": company_industry,
+            "descriptions_txt": descriptions_txt,
+            "descriptions_list": descriptions_list,
+            "job_age": job_age,
             "pros": pros_txt,
             "cons": cons_txt
         })
@@ -168,25 +202,29 @@ def pageGlassDoorScraping(url):
     return pd.DataFrame(result)
 
 
-def grassdoorUrlGenerator(n, searchWord):
-    return 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword=%22'+ searchWord +'%22&locT=C&locId=1147401,%20CA&jobType=all&fromAge=-1&minSalary=0&includeNoSalaryJobs=true&radius=100&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0&' + 'p=' + str(n)
+def grassdoor_url_generator(n, search_w):
+    return 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword=%22'+ search_w +'%22&locT=C&locId=1147401,%20CA&jobType=all&fromAge=-1&minSalary=0&includeNoSalaryJobs=true&radius=100&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0&' + 'p=' + str(n)
 
 
-def mainScraping(n, searchWord):
+def selenium_scraping(n, from_page_, to_page_, search_word_):
 
     path = os.getcwd()
     parent_path = os.path.abspath(os.path.join(path, os.pardir))
     path_folder = os.path.join(parent_path, "glassdoor-data-data-science", datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
     os.makedirs(path_folder)
 
-    urls = [grassdoorUrlGenerator(i, searchWord) for i in range(1, n+1)]
+    urls = [grassdoor_url_generator(i, search_word_) for i in range(from_page_, to_page_)]
     for inx, url in enumerate(urls):
-        df = pageGlassDoorScraping(url)
+        df = page_glassdoor_scraping_selenium(url)
         destination = os.path.join(path_folder, str(inx))
         df.to_csv(destination+'.csv')
 
     print('*'*15, " End the process ", '*'*15)
 
 
-searchWord = 'ِData Science'
-mainScraping(1, searchWord)
+search_word = 'ِData Science'
+from_page = 0
+to_page = 30
+selenium_scraping(50, from_page, to_page, search_word)
+
+
---
+++ phase-2-data-cleaning-pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+
+def create_one_csv_from_csv_folder(abs_path_folder):
+    # loop through the directory
+    all_files = [os.path.join(abs_path_folder, i) for i in os.listdir(abs_path_folder)]
+
+    # create one dataframe
+    df_result = pd.concat(map(pd.read_csv, all_files), ignore_index=True)
+
+    df_result.to_csv('.\\data\\integrated_data' + '.csv')
+
+
+# create_one_csv_from_csv_folder(r"D:\project\glassdoor-data-data-science\2022-01-19-10-55-23")
+print(os.getcwd())
+# df = pd.read_csv(".\data\integrated_data.csv")
+# print(df)
+
